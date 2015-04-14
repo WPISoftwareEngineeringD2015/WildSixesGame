71fa53a0bb18fbb5371a5f867e91f27e60f6d25f
Evin Ugur <evinoog96@gmail.com>
2015-04-14 19:47:42 -0400

    LevelScreen w actionable controls...
    .... except special moves.
    
    all the stuff is there I just need to do something with the BorderLayout
    in order to get it to be all viewable at once.

--- src/kiviuq/views/LevelView.java
+++ src/kiviuq/views/LevelView.java
@@ -2,6 +2,7 @@ package kiviuq.views;
 
 import java.awt.BorderLayout;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
@@ -13,17 +14,27 @@ public class LevelView extends JFrame{
 	 */
 	private static final long serialVersionUID = 1L;
 	Board board;
-
+	JButton back;
 	public LevelView(Board board) {
 		super();
+		setTitle("Example Level Screen");
+		setBounds(100, 100, 867, 715);
+		setLocationRelativeTo(null);
 		this.board = board;
+		
+		// TODO figure out the needed layout for this...
 		
 		JPanel grid = new BoardView(board);
 		JPanel starCriteria = new StarCriteriaView(board.getStarCriteria());
 		JPanel scoreView = new ScoreView(board);
+		back = new JButton("Back");
+		back.setSize(76, 65);
 		getContentPane().add(starCriteria, BorderLayout.NORTH);
-		getContentPane().add(scoreView, BorderLayout.AFTER_LAST_LINE);
 		getContentPane().add(grid, BorderLayout.CENTER);
+/*
+		getContentPane().add(scoreView);
+		getContentPane().add(back);
+*/
 	}
 	
 	
