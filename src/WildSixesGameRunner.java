5ce404f6ee35f862fa79c320e22c48e262d33d7e
Evin Ugur <evinoog96@gmail.com>
2015-04-15 12:59:10 -0400

    didn't mean to commit the previous change to this file

--- src/WildSixesGameRunner.java
+++ src/WildSixesGameRunner.java
@@ -1,17 +1,12 @@
-import kiviuq.entities.Board;
-import kiviuq.views.LevelScreen;
 import kiviuq.views.MainScreen;
 import kiviuq.views.SplashScreen;
-import development.Templates;
 
 
 public class WildSixesGameRunner {
 	
 	public static void main(String[] args) {
 		MainScreen mainScreen = new MainScreen();
-		LevelScreen levelScreen = new LevelScreen(new Board(Templates.getExampleTemplate()));
-		//SplashScreen s = new SplashScreen(2000, mainScreen);
-		SplashScreen s = new SplashScreen(2000, levelScreen);
+		SplashScreen s = new SplashScreen(2000, mainScreen);
 		s.launchSplashScreen();
 	}
 }
