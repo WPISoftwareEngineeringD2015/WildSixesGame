b5911da9c4b169120039769051eea727a2bcd853
jeoliva222 <jeoliva@wpi.edu>
2015-04-21 19:19:17 -0400

    Bug Fix for Probability Generator
    Updated compatibility

--- src/kiviuq/entities/Board.java
+++ src/kiviuq/entities/Board.java
@@ -22,7 +22,7 @@ public class Board {
 	boolean isMousePressed;
 	int lastX = -1;
 	int lastY = -1;
-
+	int r1,r2,r3,r4,r5;
 	public Board(LevelTemplate template) {
 		this.setTemplate(template);
 		moveLimit = template.moveLimit;
@@ -33,18 +33,18 @@ public class Board {
 		TileType[][] gridTemplate = template.getGridTemplate();
 		
 		int mult = template.probConst;
-		int r1 = 40 - mult/4;
-		int r2 = 25 - mult/10;
-		int r3 = 15 + mult/20;
-		int r4 = 10 + mult/10;
-		int r5 = 5 + mult/10;
+		r1 = 40 - mult/4;
+		r2 = 25 - mult/10;
+		r3 = 15 + mult/20;
+		r4 = 10 + mult/10;
+		r5 = 5 + mult/10;
 		
 		for (int x = 0; x < Constants.BOARD_LENGTH; x++) {
 			for (int y = 0; y < Constants.BOARD_WIDTH; y++) {
 				TileType type = gridTemplate[x][y];
 				Tile t;
 				if (type == TileType.Number)
-					t = getNextTile(r1, r2, r3, r4, r5, mode);
+					t = getNextTile();
 				else
 					t = new Tile(type); // it's a null or release tile
 				grid[x][y] = t;
@@ -120,7 +120,7 @@ public class Board {
 		return false;
 	}
 
-	public Tile getNextTile(int r1, int r2, int r3, int r4, int r5, GameMode mode) {
+	public Tile getNextTile() {
 		Random x = new Random();
 		int result = x.nextInt(99) + 1;
 		if(result < r1) {
@@ -137,7 +137,8 @@ public class Board {
 			return new Tile(1);
 		} else {
 			return new Tile(6);
-		}
+	}
+		
 		
 	//	return new Tile(Math.random() > 0.5 ? 1 : 2);
 		// for now just give us a Tile valued 1 or 2
