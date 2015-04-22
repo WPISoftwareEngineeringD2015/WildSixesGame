af7fb8f890dd5ef4e522b24fd017fd0fbb164886
mhomchenko <mhomchenko@gmail.com>
2015-04-21 23:47:50 -0400

    corrected test case package

--- src/test/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * 
- */
-/**
- * @author mhomchenko
- *
- */
-package test;
---
+++ test/kiviuq/controllers/TestRestartLevelController.java
@@ -0,0 +1,16 @@
+package kiviuq.controllers;
+
+import junit.framework.TestCase;
+
+public class TestRestartLevelController extends TestCase {
+	
+	
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+}
