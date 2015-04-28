c4b67537a96b2097f9e10a4658d7581d15d9a9e8
mhomchenko <mhomchenko@gmail.com>
2015-04-28 19:36:18 -0400

    Manually manipulate board correctly for Swap test

--- test/kiviuq/views/TestSwapTileController.java
+++ test/kiviuq/views/TestSwapTileController.java
@@ -2,10 +2,13 @@ package kiviuq.views;
 
 import development.Templates;
 import junit.framework.TestCase;
+import kiviuq.controllers.RemoveTileController;
 import kiviuq.controllers.SwapTileController;
 import kiviuq.entities.Board;
 import kiviuq.entities.LevelTemplate;
+import kiviuq.entities.MoveType;
 import kiviuq.entities.Tile;
+import kiviuq.entities.TileType;
 
 public class TestSwapTileController extends TestCase {
 	LevelScreen lvlScreen;
@@ -20,12 +23,20 @@ public class TestSwapTileController extends TestCase {
 		super.tearDown();
 	}
 	
-	public void testSwapTile(){
-		Tile initTile1 = lvlScreen.boardView.tvs[0][0].tile;
-		Tile initTile2 = lvlScreen.boardView.tvs[0][1].tile;
-		SwapTileController stc = new SwapTileController(initTile1, initTile2, lvlScreen.board, lvlScreen);
+	public void testSwapTile() throws InterruptedException{
+		Tile tile1 = new Tile(1, 1, TileType.Number);
+		Tile tile2 = new Tile(1, 1, TileType.Number);
+		Tile[][] grid = lvlScreen.board.getGrid();
+		grid[0][0] = tile1;
+		grid[1][0] = tile2;
+		lvlScreen.board.setGrid(grid);		
+		SwapTileController stc = new SwapTileController(tile1, tile2, lvlScreen.board, lvlScreen);
+		lvlScreen.board.setMoveType(MoveType.Swap);
 		stc.handleMove(null);
-		assertEquals(initTile1.getNumber(), lvlScreen.boardView.tvs[0][0].tile.getNumber());
-		assertEquals(initTile2.getNumber(), lvlScreen.boardView.tvs[0][1].tile.getNumber());
+		Thread.sleep(500);
+		assertEquals(lvlScreen.board.getGrid()[0][0].getNumber(), tile2.getNumber());
+		assertEquals(lvlScreen.board.getGrid()[0][0].getMultiplier(), tile1.getMultiplier());
+		assertEquals(lvlScreen.board.getGrid()[1][0].getNumber(), tile1.getNumber());
+		assertEquals(lvlScreen.board.getGrid()[1][0].getMultiplier(), tile1.getMultiplier());
 	}
 }
