2f21c6f487af52b80094f0fc3ef03e237fe64e1f
mhomchenko <mhomchenko@gmail.com>
2015-04-28 18:58:10 -0400

    TestAbstractMoveController
    Test that you can't make a move if you don't have any more moves -
    passing!

--- test/kiviuq/views/TestAbstractMoveController.java
+++ test/kiviuq/views/TestAbstractMoveController.java
@@ -2,10 +2,13 @@ package kiviuq.views;
 
 import development.Templates;
 import junit.framework.TestCase;
+import kiviuq.controllers.RemoveTileController;
 import kiviuq.controllers.SwapTileController;
 import kiviuq.entities.Board;
 import kiviuq.entities.LevelTemplate;
+import kiviuq.entities.MoveType;
 import kiviuq.entities.Tile;
+import kiviuq.entities.TileType;
 import kiviuq.views.LevelScreen;
 
 public class TestAbstractMoveController extends TestCase {
@@ -21,18 +24,24 @@ public class TestAbstractMoveController extends TestCase {
 		super.tearDown();
 	}
 	
-	public void testMoveAtMaxMoves(){
+	public void testMoveAtMaxMoves() throws InterruptedException{
 		int maxMoves = lvlScreen.boardView.getBoard().getMoveLimit();
 		for(int i = 0; i < maxMoves +5; i++){
 			lvlScreen.boardView.getBoard().increaseMovesMade();
 		}
 		
-		Tile initTile1 = lvlScreen.boardView.tvs[0][0].tile;
-		Tile initTile2 = lvlScreen.boardView.tvs[0][1].tile;
-		SwapTileController stc = new SwapTileController(initTile1, initTile2, lvlScreen.board, lvlScreen);
-		stc.actionPerformed(null);
-		assertEquals(initTile1.getNumber(), lvlScreen.boardView.tvs[0][0].tile.getNumber());
-		assertEquals(initTile2.getNumber(), lvlScreen.boardView.tvs[0][1].tile.getNumber());
+		Tile tile1 = new Tile(1, 1, TileType.Number);
+		Tile tile2 = new Tile(1, 1, TileType.Number);
+		Tile[][] grid = lvlScreen.board.getGrid();
+		grid[4][0] = tile1;
+		grid[5][0] = tile2;
+		lvlScreen.board.setGrid(grid);		
+		RemoveTileController rtc = new RemoveTileController(lvlScreen.board, tile2, lvlScreen);
+		lvlScreen.board.setMoveType(MoveType.Remove);
+		rtc.handleMove(null);
+		Thread.sleep(500);
+		assertEquals(lvlScreen.board.getGrid()[5][0].getNumber(), tile2.getNumber());
+		assertEquals(lvlScreen.board.getGrid()[5][0].getMultiplier(), tile2.getMultiplier());
 	}
 	
 	
@@ -42,13 +51,18 @@ public class TestAbstractMoveController extends TestCase {
 			lvlScreen.boardView.getBoard().increaseMovesMade();
 		}
 		
-		Tile initTile1 = lvlScreen.boardView.tvs[0][0].tile;
-		Tile initTile2 = lvlScreen.boardView.tvs[0][1].tile;
-		SwapTileController stc = new SwapTileController(initTile1, initTile2, lvlScreen.board, lvlScreen);
-		stc.actionPerformed(null);
+		Tile tile1 = new Tile(1, 1, TileType.Number);
+		Tile tile2 = new Tile(1, 1, TileType.Number);
+		Tile[][] grid = lvlScreen.board.getGrid();
+		grid[4][0] = tile1;
+		grid[5][0] = tile2;
+		lvlScreen.board.setGrid(grid);		
+		RemoveTileController rtc = new RemoveTileController(lvlScreen.board, tile2, lvlScreen);
+		lvlScreen.board.setMoveType(MoveType.Remove);
+		rtc.handleMove(null);
 		Thread.sleep(500);
-		assertEquals(initTile2.getNumber(), lvlScreen.boardView.tvs[0][0].tile.getNumber());
-		assertEquals(initTile1.getNumber(), lvlScreen.boardView.tvs[0][1].tile.getNumber());
+		assertEquals(lvlScreen.board.getGrid()[5][0].getNumber(), tile1.getNumber());
+		assertEquals(lvlScreen.board.getGrid()[5][0].getMultiplier(), tile1.getMultiplier());
 	}
 	
 
