1e621c0458f472ef30abbe23b1f50dfcb0d2b1b7
gtbell <gtbell@wpi.edu>
2015-04-30 19:24:37 -0400

    TestSelectScreen updated
    updated file with a test

--- test_src/kiviuq/views/TestSelectScreen.java
+++ test_src/kiviuq/views/TestSelectScreen.java
@@ -5,15 +5,21 @@ import junit.framework.TestCase;
 
 public class TestSelectScreen extends TestCase {
 	SelectScreen selectScreen;
+	MainScreen mainScreen;
 
 	protected void setUp() throws Exception {
-		selectScreen = new SelectScreen(selectScreen);
+		selectScreen = new SelectScreen(mainScreen);
 		selectScreen.setVisible(true);
 	}
 
 	protected void tearDown() throws Exception {
 		super.tearDown();
 	}
+	
+	public void testScreen(){
+		assertSame(selectScreen.lastWindow, mainScreen);
+	}
+	
 
 	
 	
