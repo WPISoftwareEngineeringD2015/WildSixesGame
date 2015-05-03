49f666c273b038db46d67f89763899710976dc96
Evin Ugur <evinoog96@gmail.com>
2015-05-03 12:30:02 -0400

    some more doc

--- src/WildSixesGameRunner.java
+++ src/WildSixesGameRunner.java
@@ -1,9 +1,15 @@
 import kiviuq.views.MainScreen;
 import kiviuq.views.SplashScreen;
 
-
+/**
+ * Main class, simply fires off a new {@link SplashScreen} reference and
+ * launches the game.
+ * 
+ * @author Evin Ugur
+ *
+ */
 public class WildSixesGameRunner {
-	
+
 	public static void main(String[] args) {
 		MainScreen mainScreen = new MainScreen();
 		SplashScreen s = new SplashScreen(500, mainScreen);
--- src/kiviuq/entities/Tile.java
+++ src/kiviuq/entities/Tile.java
@@ -1,41 +1,89 @@
 package kiviuq.entities;
 
+import kiviuq.views.TileView;
+
+/**
+ * Represents a singular Tile of any given type that is on a Board.
+ * 
+ * @author Evin Ugur
+ *
+ */
 public class Tile {
+	/** The number associated with this Tile, if any */
 	int number;
+	/** The multiplier associated with this Tile, if any */
 	int mult;
+	/** The {@link TileType type} of this Tile. */
 	TileType type;
+	/** Flag for indicating if the Tile is selected */
 	boolean isSelected;
-	
+
+	/**
+	 * Constructor for easily making new Number Tiles
+	 * 
+	 * @param number
+	 *            the number value of the new Tile
+	 * @param mult
+	 *            the multiplier
+	 */
 	public Tile(int number, int mult) {
 		this(number, mult, TileType.Number);
 	}
-	
+
+	/**
+	 * Constructor for easily making new non-Number Tiles
+	 * 
+	 * @param type
+	 *            the type associated with the new Tile
+	 */
 	public Tile(TileType type) {
 		this(-1, 1, type);
 	}
-	
-	public Tile(int number, int mult, TileType type) {
+
+	/**
+	 * Constructor for verbosely making a Tile, this is called by the other Tile
+	 * constructors
+	 * 
+	 * @param number
+	 *            the number value of the new Tile
+	 * @param mult
+	 *            the multiplier
+	 * @param type
+	 *            the type associated with the new Tile
+	 */
+	Tile(int number, int mult, TileType type) {
 		this.number = number;
 		this.mult = mult;
 		this.type = type;
 	}
+
+	/**
+	 * Convenience function to properly construct a block Tile
+	 * 
+	 * @return
+	 */
 	public static Tile NewBlockTile() {
 		return new Tile(6, 1);
 	}
-	public static Tile MakeTile(TileType t) {
-		return null;
-	}
-	
+
+	/**
+	 * @return the multiplier for this Tile
+	 */
 	public int getMultiplier() {
 		return mult;
 	}
-	
+
+	/**
+	 * The numerical value for this Tile.
+	 * 
+	 * @return The Tile's numberical value, or -1 if it does not have one.
+	 */
 	public int getNumber() {
 		return number;
 	}
-	
+
 	public void setType(TileType type) {
-		switch(type) {
+		switch (type) {
 		case Null:
 			this.type = TileType.Null;
 			break;
@@ -51,79 +99,98 @@ public class Tile {
 			break;
 		}
 	}
-	
+
+	/**
+	 * @return the {@link TileType} of the Tile.
+	 */
 	public TileType getType() {
 		return type;
 	}
-	
-//	@Override 
-//	public String toString() {
-//		String str = "";
-//		switch (type) {
-//			default: break;
-//			case Number:
-//				str = number + "";
-//			case Null:
-//				break;
-//			case Release:
-//				str = "R";
-//		}
-//		return str;
-//	}
-	
-	@Override 
+
+	@Override
 	public String toString() {
 		String str = "";
 		switch (type) {
-			default: break;
-			case Number:
-				str = "#";
-				break;
-			case Null:
-				break;
-			case Block:
-				str = "6";
-				break;
-			case Release:
-				str = "R";
-				break;
+		default:
+			break;
+		case Number:
+			str = number + "";
+			break;
+		case Null:
+			break;
+		case Block:
+			str = "6";
+			break;
+		case Release:
+			str = "R";
+			break;
 		}
 		return str;
 	}
-	
+
+	/**
+	 * A String representation of a Multiplier. Used in {@link TileView} for
+	 * displaying the multiplier value.
+	 * 
+	 * @return String representation of a Multiplier
+	 */
 	public String multToString() {
 		String multStr = "";
-		if (type == TileType.Number && number != 6 && mult > 1	) {
+		if (type == TileType.Number && number != 6 && mult > 1) {
 			multStr = "x" + mult;
 		}
 		return multStr;
 	}
-	
+
+	/**
+	 * @return true if the Tile is selected
+	 */
 	public boolean isSelected() {
 		return isSelected;
 	}
-	
+
+	/**
+	 * Sets a Tile to be selected.
+	 */
 	public void select() {
 		isSelected = true;
 	}
-	
+
+	/**
+	 * Checks to see if this Tile is selectable given in the context of a given
+	 * Game Move.
+	 * 
+	 * @param moveType
+	 *            the move being performed.
+	 * @return true if the Tile is selectable, false it otherwise. A Tile is
+	 *         selectable if its {@link Tile#type} is {@link TileType#Number}
+	 *         and the Move being performed is {@link MoveType#Remove} or
+	 *         {@link MoveType#Swap} or the Tile's {@link Tile#number} is less
+	 *         than 6.
+	 */
 	public boolean isSelectable(MoveType moveType) {
-		if (type == TileType.Release || type == TileType.Null) return false;
+		if (type == TileType.Release || type == TileType.Null)
+			return false;
 		// MoveType.Remove can Remove any Tile of type Number
-		if (moveType == MoveType.Remove) return true;
+		if (moveType == MoveType.Remove)
+			return true;
 		return number < 6 || moveType == MoveType.Swap;
 	}
-	
+
+	/**
+	 * Sets a Tile to not be selected
+	 */
 	public void unSelect() {
 		isSelected = false;
 	}
-	
-	public void toggleSelect() {
-		isSelected = !isSelected;
-	}
-	
+
+	/**
+	 * Convenience function to determine if a Tile is a block.
+	 * 
+	 * @return true if the Tile is a block, false otherwise
+	 */
 	public boolean isBlock() {
 		return type == TileType.Number && number == 6;
 	}
-	
+
 }
