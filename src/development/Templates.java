997bbeeffd906dae3d1838806a2f4fef0aa65694
mhomchenko <mhomchenko@gmail.com>
2015-05-03 01:54:39 -0400

    proper merge of templates
    restored my changes to templates which were overwritten by Gabe in
    commit 7d3df3bec979e4a585d6bb3b0dfce773bd27cd01

--- src/development/Templates.java
+++ src/development/Templates.java
@@ -6,49 +6,55 @@ import kiviuq.entities.StarCriteria;
 import kiviuq.entities.TileType;
 
 public class Templates {
-	private static LevelTemplate temp = null; // TODO
+	private static LevelTemplate elimTemp = null; // TODO
+	private static LevelTemplate lightTemp = null;
+	private static LevelTemplate puzzleTemp = null;
+	private static LevelTemplate releaseTemp = null; 
 
 	public static LevelTemplate getExampleTemplateElimination() {
-		System.out.println("getting example Elimination template");
-		StarCriteria sc = new StarCriteria(50, 100, 300, 4, 6, 8); 
-		temp = new LevelTemplate(0, GameMode.Elimination, 60, 30, 50, 60, sc);
-		return temp;
+		if (elimTemp == null) {
+			StarCriteria sc = new StarCriteria(50, 100, 300, 4, 6, 8); 
+			elimTemp = new LevelTemplate(0, GameMode.Elimination, 60, 30, 50, 60, sc);
+		}
+		return elimTemp;
 	}
 
 	public static LevelTemplate getExampleTemplateLightning() {
-		System.out.println("getting example Lightning template");
-		StarCriteria sc = new StarCriteria(0, 0, 50, 100, 300, 4); 
-		temp = new LevelTemplate(1, GameMode.Lightning, 60, 30, 50, 60, sc);
-		return temp;
+		if (lightTemp == null) {
+			StarCriteria sc = new StarCriteria(0, 0, 50, 100, 300, 4); 
+			lightTemp = new LevelTemplate(1, GameMode.Lightning, 60, 30, 50, 60, sc);
+		}
+		return lightTemp;
 	}
 
 	public static LevelTemplate getExampleTemplatePuzzle() {
-		System.out.println("getting example Puzzle template");
-		StarCriteria sc = new StarCriteria(0, 0, 50, 100, 300, 4); 
-		temp = new LevelTemplate(2, GameMode.Puzzle, 60, 30, 50, 60, sc);
-		return temp;
+		if (puzzleTemp == null) {
+			StarCriteria sc = new StarCriteria(0, 0, 50, 100, 300, 4); 
+			puzzleTemp = new LevelTemplate(2, GameMode.Puzzle, 60, 30, 50, 60, sc);
+		}
+		return puzzleTemp;
 	}
 
 	public static LevelTemplate getExampleTemplateRelease() {
-		System.out.println("getting example Release template");
-		StarCriteria sc = new StarCriteria(0, 0, 50, 100, 300, 4); 
-		temp = new LevelTemplate(3, GameMode.Release, 60, 30, 50, 60, sc);
+		if (releaseTemp == null) {
+			StarCriteria sc = new StarCriteria(0, 0, 50, 100, 300, 4); 
+			releaseTemp = new LevelTemplate(3, GameMode.Release, 60, 30, 50, 60, sc);
 
-		temp.setTileTypeAtPosition(8, 0, TileType.Null);
-		temp.setTileTypeAtPosition(8, 1, TileType.Null);
-		temp.setTileTypeAtPosition(8, 3, TileType.Null);
-		temp.setTileTypeAtPosition(8, 5, TileType.Null);
-		temp.setTileTypeAtPosition(8, 7, TileType.Null);
-		temp.setTileTypeAtPosition(8, 8, TileType.Null);
+			releaseTemp.setTileTypeAtPosition(8, 0, TileType.Null);
+			releaseTemp.setTileTypeAtPosition(8, 1, TileType.Null);
+			releaseTemp.setTileTypeAtPosition(8, 3, TileType.Null);
+			releaseTemp.setTileTypeAtPosition(8, 5, TileType.Null);
+			releaseTemp.setTileTypeAtPosition(8, 7, TileType.Null);
+			releaseTemp.setTileTypeAtPosition(8, 8, TileType.Null);
 
-		temp.setTileTypeAtPosition(0, 2, TileType.Block);
-		temp.setTileTypeAtPosition(0, 4, TileType.Block);
-		temp.setTileTypeAtPosition(0, 6, TileType.Block);
-
-		temp.setTileTypeAtPosition(8, 2, TileType.Release);
-		temp.setTileTypeAtPosition(8, 4, TileType.Release);
-		temp.setTileTypeAtPosition(8, 6, TileType.Release);
-		return temp;
+			releaseTemp.setTileTypeAtPosition(0, 2, TileType.Block);
+			releaseTemp.setTileTypeAtPosition(0, 4, TileType.Block);
+			releaseTemp.setTileTypeAtPosition(0, 6, TileType.Block);
+			
+			releaseTemp.setTileTypeAtPosition(8, 2, TileType.Release);
+			releaseTemp.setTileTypeAtPosition(8, 4, TileType.Release);
+			releaseTemp.setTileTypeAtPosition(8, 6, TileType.Release);
+		}
+		return releaseTemp;
 	}
 }
-
